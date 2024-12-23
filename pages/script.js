1aba91d4e44f4fd7678ad65a4e9bce3359f59e48
Chertopologov <Dillerfeed99@yandex.ru>
2024-12-23 22:00:09 +0300

    rotating text final fix
    its WORKING

--- pages/script.js
+++ pages/script.js
@@ -1,32 +1,22 @@
+// ROTATING TEXT
+
 const rotatingText = document.getElementById('rotatingText');
 const rotatingContainer = document.querySelector('.rotating-container');
-
 let angle = 0;
-const rotationSpeed = 2;
+const rotationSpeed = 50;
 let textRotation = 0;
-
-
 function updateTextRotation() {
     let deltaTime = 1/60;
-
-    angle = angle + rotationSpeed * deltaTime; //  угол контейнера
-
-     rotatingContainer.style.transform = `rotateY(${angle % 360}deg)`;  // вращаем контейнер
-
-    // Корректируем поворот текста
+    angle = angle + rotationSpeed * deltaTime;
       if (angle % 360 > 90 && angle % 360 < 270) {
-         textRotation = (angle + 180);
+         textRotation = -90;
+         angle = -90;
       }else{
          textRotation = angle;
        }
-
-
-    rotatingText.style.transform = `rotateY(${textRotation}deg)`;  // вращаем текст
-
-
+    rotatingText.style.transform = `rotateY(${textRotation}deg)`;
     requestAnimationFrame(updateTextRotation);
 }
-
 updateTextRotation();
 
 
@@ -37,15 +27,11 @@ function updateDateTime() {
   const now = new Date();
   const optionsDate = {  day: 'numeric', month: 'short' };
   const formattedDate = now.toLocaleDateString('en-GB', optionsDate);
-  
   const parts = formattedDate.split(' ');
-  
   if (parts.length === 2) {
        parts[1] = parts[1].toLowerCase();
   }
-
   const lowerCaseMonthDate = parts.join(' ');
-  
   dateDisplay.textContent = lowerCaseMonthDate;
 }
 setInterval(updateDateTime, 1000);
