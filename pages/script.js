777af29cfe90b940c7db03cdbb96563d1b1b0f04
Chertopologov <Dillerfeed99@yandex.ru>
2024-12-25 18:04:53 +0300

    page skroll +bg

--- pages/script.js
+++ pages/script.js
@@ -36,3 +36,123 @@ function updateDateTime() {
 }
 setInterval(updateDateTime, 1000);
 updateDateTime();
+
+// GRADIENT
+document.addEventListener('DOMContentLoaded', () => {
+  const scrollBackground = document.createElement('div');
+  scrollBackground.classList.add('scroll-background');
+  document.body.appendChild(scrollBackground);
+
+
+  const colors = [
+      'linear-gradient(to bottom, #000, #333)',
+      'linear-gradient(to bottom, #333, #666)',
+      'linear-gradient(to bottom, #666, #999)',
+      'linear-gradient(to bottom, #999, #ccc)',
+      'linear-gradient(to bottom, #ccc, #fff)',
+        'linear-gradient(to bottom, #fff, #ccc)',
+      'linear-gradient(to bottom, #ccc, #999)',
+      'linear-gradient(to bottom, #999, #666)',
+      'linear-gradient(to bottom, #666, #333)'
+  ];
+
+// SCROLL TEXT
+  let lastScrollTop = 0;
+window.addEventListener('scroll', () => {
+      let currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+
+
+    const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercentage = (currentScrollTop / scrollHeight) * 100; 
+
+      const colorIndex = Math.min(Math.floor((scrollPercentage / 100) * (colors.length -1)) , colors.length -1); 
+       scrollBackground.style.backgroundImage = colors[colorIndex];
+
+        lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; 
+    });
+
+
+});
+
+
+// SCROLL TEXT
+document.addEventListener('DOMContentLoaded', () => {
+  const mainContent = document.querySelector('.page-main-content');
+  let headerHeight = document.querySelector('.page-header').offsetHeight;
+
+
+window.addEventListener('scroll', () => {
+   let currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+
+   if(currentScrollTop > headerHeight){
+     mainContent.classList.add('visible');
+     } else{
+          mainContent.classList.remove('visible');
+     }
+  });
+}); 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mainContent = document.querySelector('.page-main-content');
+  const footer = document.querySelector('.page-footer');
+  let headerHeight = document.querySelector('.page-header').offsetHeight;
+
+
+window.addEventListener('scroll', () => {
+    let currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+
+   if(currentScrollTop > headerHeight){
+        mainContent.classList.add('visible');
+    } else{
+          mainContent.classList.remove('visible');
+     }
+
+      const documentHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.documentElement.clientHeight);
+      const windowHeight = window.innerHeight;
+    const isScrolledToBottom = (currentScrollTop + windowHeight) >= documentHeight;
+
+
+    if(isScrolledToBottom){
+       footer.classList.add('visible');
+     } else {
+       footer.classList.remove('visible')
+      }
+  });
+
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mainContent = document.querySelector('.page-main-content');
+  const header = document.querySelector('.page-header');
+  const footer = document.querySelector('.page-footer');
+
+  let headerHeight = header.offsetHeight;
+
+
+ 
+    header.classList.add('visible');
+
+  window.addEventListener('scroll', () => {
+    let currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+     if(currentScrollTop > headerHeight){
+       mainContent.classList.add('visible');
+     } else{
+        mainContent.classList.remove('visible');
+      }
+
+      const documentHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight, document.documentElement.clientHeight);
+      const windowHeight = window.innerHeight;
+    const isScrolledToBottom = (currentScrollTop + windowHeight) >= documentHeight;
+
+
+    if(isScrolledToBottom){
+       footer.classList.add('visible');
+     } else {
+       footer.classList.remove('visible')
+      }
+
+  });
+
+});
